1840af13a61eba5bde217a9ec059f5558473a902
dohaosamaa <200037696@must.edu.eg>
2026-04-27 17:26:33 +0300

    Add configuration settings for image processing

---
+++ scripts/config.py
@@ -0,0 +1,10 @@
+CONFIG = {
+    "img_size": 224,
+    "batch_size": 16,
+    "augmentation": {
+        "rotation": 0.15,
+        "zoom": 0.15,
+        "flip": True,
+        "rescale": 1./255
+    }
+}
